e5a61fab672176535978de275c903efb4819c589
mechanovkirill <mechanovkirill@gmail.com>
2023-01-09 19:40:09 +0400

    changes

--- offers/views.py
+++ offers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Offer, Countries, City
 
 CITY_SELECTED = []
+COUNTRY_SELECTED = []
 
 
 def is_valid_queryparam(param):
@@ -18,71 +19,77 @@ def listing_queryparams(request, re):
 
 
 def filters(request):
-    gender_of_living = request.GET.get('gender_of_living')
-    house_type = listing_queryparams(request, 'house_type')
-    sex = request.GET.get('sex')
-    number_of_rooms = listing_queryparams(request, 'n_rooms')
-    interior_condition = request.GET.get('interior_condition')
-    furniture = request.GET.get('furniture')
-    heating = request.GET.get('heating')
-    internet_options = listing_queryparams(request, 'internet_op')
-    number_of_living = request.GET.get('number_living')
-    free_space = request.GET.get('freespace')
-    min_cost = request.GET.get('min_cost')
-    max_cost = request.GET.get('max_cost')
-    min_rental_period = request.GET.get('min_rental_period')
-    with_animals = request.GET.get('with_animals')
-    discard_filters = request.GET.get('discard_filters')
+    try:
+        gender_of_living = request.GET.get('gender_of_living')
+        house_type = listing_queryparams(request, 'house_type')
+        sex = request.GET.get('sex')
+        number_of_rooms = listing_queryparams(request, 'n_rooms')
+        interior_condition = request.GET.get('interior_condition')
+        furniture = request.GET.get('furniture')
+        heating = request.GET.get('heating')
+        internet_options = listing_queryparams(request, 'internet_op')
+        number_of_living = request.GET.get('number_living')
+        free_space = request.GET.get('freespace')
+        min_cost = request.GET.get('min_cost')
+        max_cost = request.GET.get('max_cost')
+        min_rental_period = request.GET.get('min_rental_period')
+        with_kids = request.GET.get('with_kids')
+        with_animals = request.GET.get('with_animals')
+        discard_filters = request.GET.get('discard_filters')
 
-    qs = Offer.objects.all().filter(is_active=True).filter(city__city=CITY_SELECTED[0])
-
-    if is_valid_queryparam(sex):
-        qs = qs.filter(lodger_desired_sex=sex)
-
-    if is_valid_queryparam(gender_of_living):
-        qs = qs.filter(sex_of_living_lodgers=gender_of_living)
-
-    if house_type:
-        qs = qs.filter(house_type__in=house_type)
-
-    if number_of_rooms:
-        qs = qs.filter(number_of_rooms__in=number_of_rooms)
-
-    if is_valid_queryparam(interior_condition):
-        qs = qs.filter(interior_condition=interior_condition)
-
-    if is_valid_queryparam(furniture):
-        qs = qs.filter(furniture=furniture)
-
-    if is_valid_queryparam(heating):
-        qs = qs.filter(heating=heating)
-
-    if internet_options:
-        qs = qs.filter(internet__in=internet_options)
-
-    if is_valid_queryparam(number_of_living):
-        qs = qs.filter(count_of_living__lte=number_of_living)
-
-    if is_valid_queryparam(free_space):
-        qs = qs.filter(wanted_quantity_of_person__lte=free_space)
-
-    if is_valid_queryparam(min_cost):
-        qs = qs.filter(cost_per_person__gte=min_cost)
-
-    if is_valid_queryparam(max_cost):
-        qs = qs.filter(cost_per_person__lte=max_cost)
-
-    if is_valid_queryparam(min_rental_period):
-        qs = qs.filter(rental_period__lte=min_rental_period)
-
-    if is_valid_queryparam(with_animals):
-        qs = qs.filter(with_animals=with_animals)
-
-    if is_valid_queryparam(discard_filters):
         qs = Offer.objects.all().filter(is_active=True).filter(city__city=CITY_SELECTED[0])
 
-    return qs
+        if is_valid_queryparam(sex):
+            qs = qs.filter(lodger_desired_sex=sex)
 
+        if is_valid_queryparam(gender_of_living):
+            qs = qs.filter(sex_of_living_lodgers=gender_of_living)
+
+        if is_valid_queryparam(with_kids):
+            qs = qs.filter(with_kids=with_kids)
+
+        if house_type:
+            qs = qs.filter(house_type__in=house_type)
+
+        if number_of_rooms:
+            qs = qs.filter(number_of_rooms__in=number_of_rooms)
+
+        if is_valid_queryparam(interior_condition):
+            qs = qs.filter(interior_condition=interior_condition)
+
+        if is_valid_queryparam(furniture):
+            qs = qs.filter(furniture=furniture)
+
+        if is_valid_queryparam(heating):
+            qs = qs.filter(heating=heating)
+
+        if internet_options:
+            qs = qs.filter(internet__in=internet_options)
+
+        if is_valid_queryparam(number_of_living):
+            qs = qs.filter(number_of_lodgers__lte=number_of_living)
+
+        if is_valid_queryparam(free_space):
+            qs = qs.filter(free_space_for_num_person__lte=free_space)
+
+        if is_valid_queryparam(min_cost):
+            qs = qs.filter(cost_per_person__gte=min_cost)
+
+        if is_valid_queryparam(max_cost):
+            qs = qs.filter(cost_per_person__lte=max_cost)
+
+        if is_valid_queryparam(min_rental_period):
+            qs = qs.filter(min_rental_period__lte=min_rental_period)
+
+        if is_valid_queryparam(with_animals):
+            qs = qs.filter(with_animals=with_animals)
+
+        if is_valid_queryparam(discard_filters):
+            qs = Offer.objects.all().filter(is_active=True).filter(city__city=CITY_SELECTED[0])
+
+        return qs
+    except:
+        pass
 
 def offers(request):
     qs = None
@@ -97,6 +104,8 @@ def offers(request):
         city_qs = City.objects.all().filter(country__country=country)
         placeholder_country = f'Выбранная страна | Selected country   {country}'
         placeholder_city = 'Теперь выберите город | Now select a city'
+        COUNTRY_SELECTED.clear()
+        COUNTRY_SELECTED.append(country)
 
     if is_valid_queryparam(city):
         CITY_SELECTED.clear()
