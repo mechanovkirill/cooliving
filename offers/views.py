474ff6902c91d05a5f730148a10a1e358f1c41e5
mechanovkirill <mechanovkirill@gmail.com>
2023-01-15 23:42:35 +0400

    reserved save

--- offers/views.py
+++ offers/views.py
@@ -42,7 +42,7 @@ class HousingOfferListView(TemplateView):
         gender_of_living = self.request.GET.get('gender_of_living')
         house_type = self.listing_queryparams(self.request, 'house_type')
         sex = self.request.GET.get('sex')
-        number_of_rooms = self.listing_queryparams(self.request, 'n_rooms')
+        # number_of_rooms = self.listing_queryparams(self.request, 'n_rooms')
         interior_condition = self.request.GET.get('interior_condition')
         furniture = self.request.GET.get('furniture')
         heating = self.request.GET.get('heating')
@@ -62,45 +62,71 @@ class HousingOfferListView(TemplateView):
         elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Пол|gender')):
             qs = qs.filter(lodger_desired_sex=self.QUERY_PARAMS_DICT.get('Пол|gender'))
 
-        if self.is_valid_queryparam(sex):
-            self.QUERY_PARAMS_DICT['Пол|gender'] = sex
-            qs = qs.filter(lodger_desired_sex=self.QUERY_PARAMS_DICT.get('Пол|gender'))
-        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Пол|gender')):
-            qs = qs.filter(lodger_desired_sex=self.QUERY_PARAMS_DICT.get('Пол|gender'))
+        if self.is_valid_queryparam(self.request.GET.get('gender_of_living')):
+            self.QUERY_PARAMS_DICT['Проживают|people living'] = self.request.GET.get('gender_of_living')
+            qs = qs.filter(sex_of_living_lodgers=self.QUERY_PARAMS_DICT.get('Проживают|people living'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Проживают|people living')):
+            qs = qs.filter(sex_of_living_lodgers=self.QUERY_PARAMS_DICT.get('Проживают|people living'))
 
-        # if self.is_valid_queryparam(gender_of_living):
-        #     self.QUERY_PARAMS_DICT['gender_of_living'] = [gender_of_living, 'проживают|current lodgers gender']
-        #     qs = qs.filter(sex_of_living_lodgers=self.QUERY_PARAMS_DICT.get('gender_of_living')[0])
-        # else:
-        #     if self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('gender_of_living')[0]):
-        #         qs = qs.filter(lodger_desired_sex=self.QUERY_PARAMS_DICT.get('gender_of_living')[0])
+        if self.is_valid_queryparam(self.request.GET.get('with_kids')):
+            self.QUERY_PARAMS_DICT['С детьми|with kids'] = self.request.GET.get('with_kids')
+            qs = qs.filter(with_kids=self.QUERY_PARAMS_DICT.get('С детьми|with kids'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('С детьми|with kids')):
+            qs = qs.filter(with_kids=self.QUERY_PARAMS_DICT.get('С детьми|with kids'))
 
-        if self.is_valid_queryparam(with_kids):
-            qs = qs.filter(with_kids=with_kids)
+        if self.is_valid_queryparam(self.listing_queryparams(self.request, 'house_type')):
+            self.QUERY_PARAMS_DICT['Тип жилья|housing type'] = self.listing_queryparams(self.request, 'house_type')
+            qs = qs.filter(house_type__in=self.QUERY_PARAMS_DICT.get('Тип жилья|housing type'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Тип жилья|housing type')):
+            qs = qs.filter(house_type__in=self.QUERY_PARAMS_DICT.get('Тип жилья|housing type'))
 
-        if house_type:
-            qs = qs.filter(house_type__in=house_type)
+        if self.is_valid_queryparam(self.listing_queryparams(self.request, 'n_rooms')):
+            self.QUERY_PARAMS_DICT['Количество комнат|number of rooms'] = self.listing_queryparams(self.request, 'n_rooms')
+            qs = qs.filter(number_of_rooms__in=self.QUERY_PARAMS_DICT.get('Количество комнат|number of rooms'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Количество комнат|number of rooms')):
+            qs = qs.filter(number_of_rooms__in=self.QUERY_PARAMS_DICT.get('Количество комнат|number of rooms'))
 
-        if number_of_rooms:
-            qs = qs.filter(number_of_rooms__in=number_of_rooms)
+        if self.is_valid_queryparam(self.request.GET.get('interior_condition')):
+            self.QUERY_PARAMS_DICT['Отделка|interior'] = self.request.GET.get('interior_condition')
+            qs = qs.filter(interior_condition=self.QUERY_PARAMS_DICT.get('Отделка|interior'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Отделка|interior')):
+            qs = qs.filter(interior_condition=self.QUERY_PARAMS_DICT.get('Отделка|interior'))
 
-        if self.is_valid_queryparam(interior_condition):
-            qs = qs.filter(interior_condition=interior_condition)
+        if self.is_valid_queryparam(self.request.GET.get('furniture')):
+            self.QUERY_PARAMS_DICT['Мебель|furniture'] = self.request.GET.get('furniture')
+            qs = qs.filter(furniture=self.QUERY_PARAMS_DICT.get('Мебель|furniture'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Мебель|furniture')):
+            qs = qs.filter(furniture=self.QUERY_PARAMS_DICT.get('Мебель|furniture'))
 
-        if self.is_valid_queryparam(furniture):
-            qs = qs.filter(furniture=furniture)
+        if self.is_valid_queryparam(self.request.GET.get('heating')):
+            self.QUERY_PARAMS_DICT['Отопление|heating'] = self.request.GET.get('heating')
+            qs = qs.filter(heating=self.QUERY_PARAMS_DICT.get('Отопление|heating'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Отопление|heating')):
+            qs = qs.filter(heating=self.QUERY_PARAMS_DICT.get('Отопление|heating'))
 
-        if self.is_valid_queryparam(heating):
-            qs = qs.filter(heating=heating)
+        if self.is_valid_queryparam(self.listing_queryparams(self.request, 'internet_op')):
+            self.QUERY_PARAMS_DICT['Интернет|internet'] = self.listing_queryparams(self.request, 'internet_op')
+            qs = qs.filter(internet__in=self.QUERY_PARAMS_DICT.get('Интернет|internet'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Интернет|internet')):
+            qs = qs.filter(internet__in=self.QUERY_PARAMS_DICT.get('Интернет|internet'))
 
-        if internet_options:
-            qs = qs.filter(internet__in=internet_options)
+        if self.is_valid_queryparam(self.request.GET.get('number_living')):
+            self.QUERY_PARAMS_DICT['Мак.проживающих|Max.lodgers'] = self.request.GET.get('number_living')
+            qs = qs.filter(number_of_lodgers__lte=self.QUERY_PARAMS_DICT.get('Мак.проживающих|Max.lodgers'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Мак.проживающих|Max.lodgers')):
+            qs = qs.filter(number_of_lodgers__lte=self.QUERY_PARAMS_DICT.get('Мак.проживающих|Max.lodgers'))
 
-        if self.is_valid_queryparam(number_of_living):
-            qs = qs.filter(number_of_lodgers__lte=number_of_living)
+        if self.is_valid_queryparam(self.request.GET.get('freespace')):
+            self.QUERY_PARAMS_DICT['Свободно мест|free space'] = self.request.GET.get('freespace')
+            qs = qs.filter(free_space_for_num_person__lte=self.QUERY_PARAMS_DICT.get('Свободно мест|free space'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Свободно мест|free space')):
+            qs = qs.filter(free_space_for_num_person__lte=self.QUERY_PARAMS_DICT.get('Свободно мест|free space'))
 
-        if self.is_valid_queryparam(free_space):
-            qs = qs.filter(free_space_for_num_person__lte=free_space)
+        if self.is_valid_queryparam(self.request.GET.get('min_cost')):
+            self.QUERY_PARAMS_DICT['Минимальная цена|min. price'] = self.request.GET.get('min_cost')
+            qs = qs.filter(cost_per_person__gte=self.QUERY_PARAMS_DICT.get('Свободно мест|free space'))
+        elif self.is_valid_queryparam(self.QUERY_PARAMS_DICT.get('Свободно мест|free space')):
+            qs = qs.filter(cost_per_person__gte=self.QUERY_PARAMS_DICT.get('Свободно мест|free space'))
 
         if self.is_valid_queryparam(min_cost):
             qs = qs.filter(cost_per_person__gte=min_cost)
